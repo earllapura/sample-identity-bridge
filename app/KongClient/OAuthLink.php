d2ac2666a152aac12f0d8af32b6f9aa016e90ba2
earllapura <earlvon.lapura@g.msuiit.edu.ph>
2018-08-22 05:43:21 +0000

    Added capability to process multiple scopes

--- app/KongClient/OAuthLink.php
+++ app/KongClient/OAuthLink.php
@@ -4,6 +4,7 @@ namespace App\KongClient;
 
 use App\KongClient\Contracts\OAuthLinkInterface;
 use GuzzleHttp\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class OAuthLink implements OAuthLinkInterface
 {
@@ -21,13 +22,11 @@ class OAuthLink implements OAuthLinkInterface
      */
     public function getClientInfo(string $clientId)
     {
-        return $this->queryGatewayFirst(
+        $response = $this->queryGateway(
             '/oauth2',
-            [
-                'client_id' => $clientId,
-            ],
-            'client'
+            [ 'client_id' => $clientId, ]
         );
+        return $this->formatDataToJson($response, 'client', 0);
     }
 
     /**
@@ -35,33 +34,56 @@ class OAuthLink implements OAuthLinkInterface
      */
     public function getScopeInfo(string $scopeName)
     {
-        return $this->queryGatewayFirst(
+        $response = $this->queryGateway(
             '/auth/scopes',
-            [
-                'scope_name' => $scopeName,
-            ],
-            'scope'
+            [ 'scope_name' => $scopeName, ]
         );
+        return $this->formatDataToJson($response, 'scopes');
     }
 
     /**
      * Helper function to get first result of a gateway query
      * @param  string $path            The relative path on the gateway
      * @param  array  $queryParameters The query parameters
-     * @param  string $dataKey         The key for the data object
-     * @return object                  A standard object with top level attribute
-     *                                 <code>statusCode</code> for HTTP status code and
-     *                                 <code>dataKey</code> with object containing the data
+     * @return ResponseInterface       The response from the gateway
      */
-    private function queryGatewayFirst(string $path, array $queryParameters, string $dataKey)
+    private function queryGateway(string $path, array $queryParameters)
     {
-        $response = $this->client->request(
+        return $this->client->request(
             'GET',
             config('api.gateway') . $path,
             $queryParameters
         );
+    }
+
+    /**
+     * Formats raw data to a standard object with status code and data
+     * @param  ResponseInterface $response The Guzzle Response object
+     * @param  string            $dataKey  The data key 
+     * @param  int|null          $index    The index of the object in the data array.
+     *                                     Null means all.
+     * @return object                      A standard object with top level attribute
+     *                                     <code>statusCode</code> for HTTP status code and
+     *                                     <code>dataKey</code> with object containing the data
+     */
+    private function formatDataToJson(ResponseInterface $response, string $dataKey, int $index=null)
+    {
         $parsed = json_decode($response->getBody());
-        $data = (!empty($parsed) && property_exists($parsed, 'data')) ? $parsed->data[0] : null;
+        $data = $this->getValueOfData($parsed, $index);
         return (object)[$dataKey=>$data, 'statusCode'=>$response->getStatusCode()];
+    }
+
+    /**
+     * Gets the true value of the data, replacing non-existent values with null, and
+     * returns data based on index.
+     * @param  object   $parsedResponse The response object
+     * @param  int|null $index          The index of the data in the data array
+     * @return object|null              The parsed data object
+     */
+    private function getValueOfData(object $parsedResponse, int $index=null)
+    {
+        if(!empty($parsed) && property_exists($parsed, 'data')) return null;
+        if(is_null($index)) return $parsedResponse->data;
+        return $parsedResponse->data[$index];
     }
 }
--- tests/Feature/OAuthLinkTest.php
+++ tests/Feature/OAuthLinkTest.php
@@ -7,8 +7,6 @@ use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Response;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class OAuthLinkTest extends TestCase
@@ -19,11 +17,26 @@ class OAuthLinkTest extends TestCase
      */
     public function testGetsClientInfo()
     {
-        $unsuccessfulResponse = new Response(206, ['Content-Type' => 'application/json'], '{"total":1,"data":[{"created_at":1534239617000,"client_id":"test_app","id":"12121212121","redirect_uri":["http:\/\/mockbin.org\/"],"name":"Test Application","client_secret":"testapp123","consumer_id":"212121"}]}');
-        $mock = new MockHandler([$unsuccessfulResponse]);
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler'=>$handler]);
-        $apiLink = new OAuthLink($client);
+        $unsuccessfulResponse = new Response(206, ['Content-Type' => 'application/json'], '
+            {
+                "total":1,
+                "data":[
+                    {
+                        "created_at":1534239617000,
+                        "client_id":"test_app",
+                        "id":"12121212121",
+                        "redirect_uri":["http:\/\/mockbin.org\/"],
+                        "name":"Test Application",
+                        "client_secret":"testapp123",
+                        "consumer_id":"212121"
+                    }
+                ]
+            }'
+        );
+        $mock       = new MockHandler([$unsuccessfulResponse]);
+        $handler    = HandlerStack::create($mock);
+        $client     = new Client(['handler' => $handler]);
+        $apiLink    = new OAuthLink($client);
         $expectedId = "12121212121";
         $clientInfo = $apiLink->getClientInfo("test_app");
         $this->assertEquals($expectedId, $clientInfo->client->id);
@@ -35,21 +48,27 @@ class OAuthLinkTest extends TestCase
      */
     public function testGetsScopeInfo()
     {
-        $scopeName = "testscope";
-        $response = new Response(200, ['Content-Type' => 'application/json'], 
+        $scopeName       = "testscope scopetest";
+        $scopeDataString = '[
+            {
+                "name": "testscope",
+                "description": "Lorem ipsum desu ka"
+            }, {
+                "name": "scopetest",
+                "description": "Lorem ipsum desu"
+            }
+        ]';
+        $response = new Response(200, ['Content-Type' => 'application/json'],
             '{
-                "data": [{
-                    "name": "'.$scopeName.'",
-                    "description": "Lorem ipsum desu ka"
-                }]
+                "data": ' . $scopeDataString . '
             }'
         );
-        $mock = new MockHandler([$response]);
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler'=>$handler, 'http_errors'=>false]);
-        $apiLink = new OAuthLink($client);
-        $scopeInfo = $apiLink->getScopeInfo("test_app");
-        $this->assertEquals($scopeName, $scopeInfo->scope->name);
+        $mock      = new MockHandler([$response]);
+        $handler   = HandlerStack::create($mock);
+        $client    = new Client(['handler' => $handler, 'http_errors' => false]);
+        $apiLink   = new OAuthLink($client);
+        $scopeInfo = $apiLink->getScopeInfo($scopeName);
+        $this->assertEquals(json_decode($scopeDataString), $scopeInfo->scopes);
         $this->assertEquals(200, $scopeInfo->statusCode);
     }
 }
