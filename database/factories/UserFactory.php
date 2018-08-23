48e18ec626571d57f0fc4704852c5841225aca51
earllapura <earlvon.lapura@g.msuiit.edu.ph>
2018-08-23 05:45:23 +0000

    Added username to user attributes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,13 +11,16 @@ use Faker\Generator as Faker;
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
 |
-*/
+ */
 
 $factory->define(App\User::class, function (Faker $faker) {
+    $email    = $faker->unique()->safeEmail;
+    $username = explode("@", $email)[0];
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'name'           => $faker->name,
+        'username'       => $username,
+        'email'          => $email,
+        'password'       => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', //secret
         'remember_token' => str_random(10),
     ];
 });
